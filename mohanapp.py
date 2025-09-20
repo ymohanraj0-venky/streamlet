99633c17d72945b075fe49c6ff3d00d3ab8f79b4
ymohanraj0-venky <ymohanraj0@gmail.com>
2025-09-20 12:45:54 +0530

    Update mohanapp.py

--- mohanapp.py
+++ mohanapp.py
@@ -5,12 +5,26 @@ name=st.text_input("Full Name","Mohan Raj")
 age=st.number_input("Age", min_value=1, max_value=100, value=25)
 gender=st.selectbox("Gender",["Male", "Female", "Others"])
 dob=st.data_input("Data of Birth")
-
 st.header("Contact Information")
 email=st.text_input("Email","mohan@example.com")
 phone=st.text_input("Phone Number","+91-1234567891")
 address=st.text_area("Address","123,Main Street,City,Country")
-
 st.header("Education")
-education=st.text_area("Education Background","B.sc")
-
+education=st.text_area("Education Background","B.sc in Computer")
+st.header("Skills")
+skills=st.multiselect("Select Skill",
+                      ["Python", "Machine learning", Data Analysis","Web Development","Communication"],
+                                      default=["Select your skills"]
+st.header("Biodata Summary")
+if st.button("Generate Biodata"):
+   st.subheader("Here's your Biodata:")
+  st.write(f"**Name:**{name}")
+  st.write(f"**Age:**{age}")
+  st.write(f"**Gender:**{gender}")
+  st.write(f"**Data of Birth:**{dob}") 
+  st.write(f"**Email:**{email}")
+  st.write(f"**Phone Number:**{phone number}")
+  st.write(f"**Adderss:**{address}")
+  st.write(f"**Education:**{education}")
+  st.write(f"**Skills:**{','.join(skills)}")
+  
